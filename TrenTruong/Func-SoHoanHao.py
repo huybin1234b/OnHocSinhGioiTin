c83f8234495d30592e5ae7792a67a6a3eaddd454
M17 <officalhuybin1234b@duck.com>
2024-12-02 14:15:09 +0700

    push de tren truong

---
+++ TrenTruong/Func-SoHoanHao.py
@@ -0,0 +1,10 @@
+def songuyento(n):
+    kiemtra = False
+    uoc = 0
+    for n in range(1,n+1):
+        if n % i == 0
+    if uoc == 2:
+       kiemtra = True
+       return kiemtra
+n =int(input("nhập số n "))
+if songuyento(n)
